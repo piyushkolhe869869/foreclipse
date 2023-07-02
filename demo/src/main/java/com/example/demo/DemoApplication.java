34d75cb8e8a8e65dd7129f7db6aa04a51e5aaefb
piyushkolhe869869 <kolhepiyush222@gmail.com>
2023-07-02 17:28:20 +0530

    edited for piyu

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,10 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+
+		for(int i=1;i<=10:i++){
+			System.out.println(hii);
+		}
 	}
 
 }
