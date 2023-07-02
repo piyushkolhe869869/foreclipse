b66109958d171049862b29ff3c51bbdcb8861f41
piyushkolhe869869 <kolhepiyush222@gmail.com>
2023-07-02 17:34:41 +0530

    edited by shubham

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -9,8 +9,10 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 
-		for(int i=1;i<=10:i++){
-			System.out.println(hii);
+		for(int i=1;i<=10;i++){
+			System.out.println(hiiii);
+          System.out.println(bye);
+
 		}
 	}
 
